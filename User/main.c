278ad72e47cf959ad22faac06ae08a87f3db5ab1
caft <caft0505@gmail.com>
2024-11-23 22:01:59 +0800

    1. refine project layout; 2. add led flash when serial data arrive

--- User/main.c
+++ User/main.c
@@ -1,9 +1,11 @@
-#include "stm32f10x.h"                  // Device header
-#include "Delay.h"
-#include "Serial.h"
-#include "PWM.h"
-#include "midi.h"
 #include <string.h>
+#include "stm32f10x.h"
+#include "delay.h"
+#include "serial.h"
+#include "pwm.h"
+#include "led.h"
+
+#include "midi.h"
 
 #define MIDI_MAGIC 0xbeefu
 
@@ -12,7 +14,6 @@ void decodePayload(uint8_t byte);
 void buzzerPlay(uint32_t us, uint32_t frequency, uint32_t duty);
 void onMidiEvent(midi_context_t *ctx, midi_event_t *event);
 void onMidiComplete(midi_context_t *ctx);
-void delay(uint32_t us);
 
 typedef struct {
     uint16_t magic;
@@ -68,21 +69,13 @@ void onReadable(uint8_t byte)
     gDecodeFunc(byte);
 }
 
-void delay(uint32_t us)
-{
-    if (us == 0) {
-        return;
-    }
-    if (us < 233015) {
-        Delay_us(us);
-    } else {
-        Delay_ms(us/1000);
-    }
-}
-
 void buzzerPlay(uint32_t us, uint32_t frequency, uint32_t duty)
 {
-    delay(us);
+    delay_us(us);
+
+    // the sysclock is 72MHz,
+    // and prescaler=72 is set in pwm.c,
+    // so we use 1000000 = 72MHz/72
     uint32_t period = 1000000 / frequency;
     PWM_SetAutoreload(period - 1);
     uint32_t compareValue = (period * duty) / 100;
@@ -93,7 +86,7 @@ void onMidiEvent(midi_context_t *ctx, midi_event_t *event)
 {
     // simpler is better:
     // just focus on&off, ignore others
-    // just play channel 0, ignore others
+    // just play one channel, ignore others
 
     uint8_t channel = event->status & 0x0f;
     if (channel != gMessage.header.channel_id) {
@@ -105,6 +98,7 @@ void onMidiEvent(midi_context_t *ctx, midi_event_t *event)
     uint8_t type = event->status & 0xf0;
     if (type == NOTE_ON || type == NOTE_OFF) {
         uint32_t freq = midi_note_to_freq(event->param1);
+        // the max duty is 127 in MIDI
         uint32_t duty = event->param2;
         if (duty > 127) {
             duty = 127;
@@ -114,7 +108,7 @@ void onMidiEvent(midi_context_t *ctx, midi_event_t *event)
         duty = duty / 127. * 100;
         buzzerPlay(delta, freq, duty / 3);
     } else {
-        delay(delta);
+        delay_us(delta);
     }
 }
 
@@ -130,6 +124,8 @@ void onMidiComplete(midi_context_t *ctx)
 
 int main(void)
 {
+    LED_Init();
+
     uint8_t i = 0;
 
     gDecodeFunc = decodeHeader;
@@ -139,29 +135,28 @@ int main(void)
     gMidiCtx.on_event = onMidiEvent;
     gMidiCtx.on_complete = onMidiComplete;
 
+    // Test C4 Scale Notes
 //    int _c[] = {262, 294, 330, 349, 392, 440, 494};
 //    for (i = 0; i < sizeof(_c)/sizeof(_c[0]); ++i)
 //    {
 //        buzzerPlay(300000, _c[i], 30);
 //    }
-    
 //    int c[] = {523, 587, 659, 698, 784, 880, 988};
 //    for (i = 0; i < sizeof(c)/sizeof(c[0]); ++i)
 //    {
 //        buzzerPlay(300000, c[i], 30);
 //    }
-
 //    int c_[] = {1046, 1175, 1318, 1497, 1568, 1760, 1976};
 //    for (i = 0; i < sizeof(c_)/sizeof(c_[0]); ++i)
 //    {
 //        buzzerPlay(300000, c_[i], 30);
 //    }
-
 //    buzzerPlay(0, 100, 0);
     
     while (1)
     {
         if (gHasNewMessage) {
+            LED_Flash();
             int ret = midi_decode(&gMidiCtx, gMessage.payload, gMessage.header.payload_size);
             while (ret != MIDI_OK);
             gHasNewMessage = 0;
